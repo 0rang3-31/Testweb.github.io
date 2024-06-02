8e184496bf3426f566ef462b074ea9a9ddc6a99c
0rang3-31 <121766562+0rang3-31@users.noreply.github.com>
2024-06-02 11:01:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showVideos() {
+    document.querySelector('.message').style.display = 'none';
+    document.querySelector('.videos-button').style.display = 'none';
+    document.getElementById('video-grid').style.display = 'grid';
+}
